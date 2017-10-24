be66a9f7b95662ccd3d15ce1616d1c0cbc5f6830
MelanieOC <melanie.ocharan@gmail.com>
2017-10-24 16:19:07 -0500

    background

--- src/App.js
+++ src/App.js
@@ -6,13 +6,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
+        <p className="App-intro" style={{color: 'white'}}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        
+        <iframe src="https://open.spotify.com/embed/track/79cuOz3SPQTuFrp8WgftAu" style={{position:'fixed', bottom:0, left:0}} width="100%" height="80" frameborder="0" allowtransparency="true"></iframe>
       </div>
     );
   }
