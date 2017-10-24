9305e74c4587656adcb648dd54fac86288722976
AQP20171679 <alejandraadriantejada@gmail.com>
2017-10-24 17:21:50 -0500

    vista costado

--- src/Album.js
+++ src/Album.js
@@ -2,92 +2,63 @@ import React, { Component } from 'react';
 import './Album.css';
 
 class Album extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      play: 'REPRODUCIR',
+      follow: 'FOLLOW'
+    };
+    this.myCounter = 0;
+  }
   render() {
+
+    const changePlay = () => {
+      if (this.state.play == 'REPRODUCIR') {
+        this.setState({
+          play: 'PAUSE'
+        });
+      }
+      else {
+        this.setState ( {
+          play: 'REPRODUCIR',
+        });
+      }
+    }
+    const changeFollow = () => {
+      if (this.state.follow == 'FOLLOW') {
+        this.setState({
+          follow: 'UNFOLLOW'
+        });
+      }
+      else {
+        this.setState ( {
+          follow: 'FOLLOW',
+        });
+      }
+    }
     return (
-      <div className= "col-xs-12 col-lg-3 col-xl-4 col-sticky">
-        <header className="entity-info">
-          <div className= "media-object">
-            <div className= "media-object-hoverable">
-              <div className = "react-contextmenu-wrapper">
-                <div className = "cover-art shadow light actionable" aria-hidden="true">
-                  <div>
-                    <div className = "icon">
-                      <svg width="80" height="81" viewBox="0 0 80 81" xmlns="http://www.w3.org/2000/svg">
-                      <title>
-                        Playlist Icon                      
-                      </title>
-                      <path rule="evenodd">
-
-                      </path>
-
-                      </svg>
-
-                    </div>
-                    <div className = "cover-art-image conver-art-image-loaded" style="background-image: url(https://i.scdn.co/image/7ddc3b84f82559787451d714e308580a1cfc)">
-
-                    </div>
-                    <button className = "cover-art-playback">
-                      <svg className = "icon-play" viewBox="0 0 85 100">
-                        <path fill="currentColor" d="M81 444">
-                          <title>REPRODUCIR</title>
-
-                        </path>
-
-                      </svg>
-                    </button>
-                  </div>
-
-                </div>
-                <div className="mo-info">
-                  <div className = "react-contextmenu-wrapper">
-                    <div className = "mo-info-name" title="Made In Perú">Made In Perú
-
-                    </div>
-
-                  </div>
-
-                </div>
-
+      <div className='row'>
+        <div className="col-xs-12 col-md-4 col-lg-3 col-xl-4 col-sticky">
+          <div className='row'>
+            <div className="col-xs-4 col-md-12 col-lg-3 col-xl-4 col-sticky">
+              <img className='img-100' src='https://i.scdn.co/image/8fceebd160a5efab9088f7a66e0555d70b44a520' />
+            </div>
+            <div className="col-xs-8 col-md-12 col-lg-7 col-xl-8 col-sticky">
+              <h2>ELECTRO BR</h2>
+              <p>By<b>Spotify</b></p>
+              <p>50 SONGS</p>
+              <div>
+                <button className='btn btn-green' onClick={() => changePlay()}>{this.state.play}</button>
+                <button className='btn btn-trasparent' onClick={() => changeFollow()}>{this.state.follow}</button>
+                <button className='btn btn-trasparent'><img src='' /></button>
               </div>
-            <div className = "no-meta ellipsis-one-line">
-              <span>
-                "De"
-              </span>
-              <span>
-                <a href="/user/spotify">Spotify</a>
-              </span>
             </div>
-            </div>
-            <div className ="media-bd">
-              <div className = "entity-name">
-                <h2>Made In Perú</h2>
-                <span>"De"</span>
-                <span>
-                  <a>
-                    Spotify
-                  </a>
-                </span>
-              </div>
-
-            </div>
-            <p className = "entity-long-description">"!Conoce el pasado, presente y futuro del sonido que ha puesto a Perú en el mapa musical global! Foto:Alejandro y Maria Laura"</p>
-            <p ClassName = "text-silence entity-additional-info">"60 canciones"</p>
-            <button className = "btn btn-green">REPRODUCIR</button>
-            <div className = "follow horizontal-list">
-              <button className = "btn btn-transparent">SEGUIR</button>
-              <span className = "react-contextmenu-wrapper">
-                <button className= "btn btn-transparent btn--narrow">
-                  <div className= "spoticon-ellipsis-24">
-
-                  </div>
-                </button>
-              </span>
-            </div>
-
           </div>
-        </header>
+        </div >
+
       </div>
     );
+
   }
 }
 
