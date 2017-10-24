db0340803e49f54915e079390cf24f300c7d0757
KelyVargaya <you@example.com>
2017-10-24 16:33:02 -0500

    Maquetacion de la listita

--- src/App.js
+++ src/App.js
@@ -2,17 +2,31 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const List = () =>{
+	return (<div>
+            <div className="container">
+              <div class="row">
+                <div class="col-md-4">Aqui ira el album</div>
+                  <div class="col-md-8 list">
+                      <li><div className="id">
+                        <span className="number">1.</span>
+                        <span className="glyphicon glyphicon-play " id="play" aria-hidden="true"></span>
+                        </div>
+                          <div className="titulo">Primera cancion</div>
+                          <div className="duracion"><span> 3.22 </span></div> 
+                      </li> 
+                  </div>
+                </div>
+              </div>
+            </div>
+	);
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <List/>
       </div>
     );
   }
