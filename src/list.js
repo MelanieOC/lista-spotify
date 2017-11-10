9bd1fdf554c37cc4f9b228ce7609bea59c35e649
luisaGonzales <luisagc711@gmail.com>
2017-11-10 16:00:55 -0500

    Modificaciones

--- src/list.js
+++ src/list.js
@@ -6,7 +6,7 @@ const List = ({ title, artist, duration, index, currentIndex, play }) => {
     const comparar = currentIndex === index - 1;
     return (
       <div className="list">
-        <div className={comparar ? 'list__id list__id--green' : 'list__id'}>
+        <div className={comparar ? 'list__id list--green' : 'list__id'}>
           {
             comparar && play ?
               <span className="glyphicon glyphicon-volume-up list__number" aria-hidden="true"></span> :
@@ -16,19 +16,19 @@ const List = ({ title, artist, duration, index, currentIndex, play }) => {
           <button className='list__play' onClick={comparar && play ? () => pause() : () => nextSong(index - 1)}>
             {
               comparar && play ?
-                <span className="glyphicon glyphicon-pause green" aria-hidden="true"></span> :
+                <span className="glyphicon glyphicon-pause list--green" aria-hidden="true"></span> :
                 <span className="glyphicon glyphicon-play" aria-hidden="true"></span>
             }
           </button>
         </div>
 
         <div className="list__title">
-          <strong className={comparar ? 'green' : ''}>{title}</strong>
+          <strong className={comparar ? 'list--green' : ''}>{title}</strong>
           <p>{artist}</p>
         </div>
 
         <div className="list__duration">
-          <span className={comparar ? 'green' : ''}> {duration}</span>
+          <span className={comparar ? 'list--green' : ''}> {duration}</span>
         </div>
       </div>
     );
